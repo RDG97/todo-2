9197ff74874f6e23f6d9c3806f7ab8159a72ebcf
RDG97 <ryangann1997@gmail.com>
2022-10-20 19:41:22 +0000

    set up react an bootstrap

---
+++ app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <div className="row bg-danger">test</div>
+  </React.StrictMode>
+);
