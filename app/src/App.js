56cddb5dbec9210eae0780c2bc51307a2fff2aca
RDG97 <ryangann1997@gmail.com>
2022-10-21 02:16:53 +0000

    modified:   app/src/App.js modified:   app/src/Notdone.js

--- app/src/App.js
+++ app/src/App.js
@@ -4,9 +4,10 @@ import { useState, useEffect } from "react"
 import Filt from "./Notdone"
 export default function App() {
     const [data, setData] = useState([]);
+    const [state, setState] = useState('');
     return(
         <>
     <Enter data={data} setData={setData}/>
-    <Filt data={data} setData={setData}/>
+    <Filt data={data} setData={setData} state={state} setState={setState}/>
     </>
 )}
--- app/src/Notdone.js
+++ app/src/Notdone.js
@@ -6,17 +6,12 @@ export default function Filt(props) {
 
     
     console.log('before filt', props.data)
+
      let notdone = props.data.filter(data =>
-        data[2] === 'notdone'
+        data.stat === 'notdone'
         
         );
         
-        console.log('notdone', notdone)
-
-
-
-
-
     return (
 
         <div className="container bg-primary" id="listBody">
@@ -24,15 +19,16 @@ export default function Filt(props) {
                     <div className="col bg-danger border border-2 border-dark">Coming Up
                     <div className="row bg-warning border border-2">
                     {notdone.map((props, index) => (
-                    <>
-                    <h1>{props[0]}</h1>
-                    {console.log('commers mapped index', index)}
+                    <div key={props.help}>
+                        
+                    <h1 >{props.help}</h1>
+                    {console.log('Notdone mapped index', index)}
                     <input type='submit' onClick={ ()=> test(index, props)} ></input>
     
-                    </>
+                    </div>
                     ))} 
                     </div>
                     </div>
                     </div>
                     </div>
-        )};
+        )}
